67fbcb6d6925a7828b36b16519dabb9f6a90c0db
rohansas <rohansas@protonmail.com>
2020-07-29 12:11:03 +0530

    check

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const server=http.createServer(app)
 const listener=server.listen(port,hostname,()=>{console.log("Server spinned up on port "+listener.address().port)});
 
 app.get("/",(req,res,next)=>{
-    res.send("randiaap")
+    res.send("randi")
 })
