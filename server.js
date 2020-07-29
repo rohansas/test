de69b3b294e7a11d6615e36b2ef1adb83ba1d464
rohansas <rohansas@protonmail.com>
2020-07-29 12:22:28 +0530

    check

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ const port=process.env.PORT || 8021;
 const app=express();
 const server=http.createServer(app)
 const listener=server.listen(port,hostname,()=>{console.log("Server spinned up on port "+listener.address().port)});
-
+const https=require("https")
 app.get("/",(req,res,next)=>{
-    res.send("randi")
+    https.get('http://13.233.2.27:5000', {headers:{ 'User-Agent': 'Node Server' }},(resp) => {
+        let pythonData = '';
+      
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          pythonData += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          pythonData=JSON.parse(pythonData)
+          res.send(pythonData)
+          res.end()
+        });
+      
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+
 })
